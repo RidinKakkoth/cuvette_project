5497061f858edfe0ecb3c69de570c67c3cdb97f7
Ridin Kakkoth <k.ridin@ymail.com>
2024-08-30 13:03:28 +0530

    fix error in dashboard

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import jwt from 'jsonwebtoken'
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+
+    const token = localStorage.getItem('token'); 
+    const decoded=jwt.decode(token)
+    const role=decoded.role
+    if (token) {
+      router.push(`/dashboard?role=${role}`);
+    } else {
+      router.push('/signin');
+    }
+  }, [router]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      its our home 
-       
+      <p>Loading...</p>
     </main>
   );
 }

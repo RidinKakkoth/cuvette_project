1806d5526d3dc09784d2534d478829f3e4a4bb1e
Ridin Kakkoth <k.ridin@ymail.com>
2024-08-30 14:52:06 +0530

    spinner

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,10 +2,17 @@
 
 import { Suspense } from 'react';
 import DashboardContent from '../components/DashboardContent.jsx'; 
+import { ClipLoader } from "react-spinners";
 
 export default function Dashboard() {
   return (
-    <Suspense fallback={<div>Loading dashboard...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          <ClipLoader color="#4A90E2" loading={true} size={50} />
+        </div>
+      }
+    >
       <DashboardContent />
     </Suspense>
   );
